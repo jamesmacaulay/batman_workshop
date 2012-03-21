a2da53cfc9b1814e6f556e4e640de2280672874f
James MacAulay <james@shopify.com>
2012-03-21 19:06:36 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BatmanWorkshop::Application.config.secret_token = 'ee35bd5461a8773c159b478225a19572c3e84531f00c1760b917e80f82b0c0a190a7a8659877312d9c296aadc101152507ebd711363a059a16382fdaf177a393'
